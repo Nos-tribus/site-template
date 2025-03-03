30491a229b6a7c191587053d12c5f8682614eb52
Gilles FRANCOIS <gilles@sopress.net>
2025-03-03 10:42:35 +0100

    intro

--- src/pages/intro/intro.js
+++ src/pages/intro/intro.js
@@ -1,21 +1,30 @@
 App.modules["intro"] = (() => {
   return {
+    async handleIntro() {
+      const lien = document.querySelector('[href*="/intro"]');
+      const url = lien.href;
+      if (!localStorage.getItem("intro")) {
+        localStorage.setItem("intro", true);
+        window.location.href = url;
+      }
+    },
     start() {
+      this.handleIntro();
       const video = document.querySelector(".video video");
-      if(!video) return;
-      
-        // Définir le volume initial à 0
-        video.volume = 0;
+      if (!video) return;
 
-        video.addEventListener("mouseenter", () => {
-          console.log("mouseenter");
-            // video.volume = 0.5; // Volume à 50% (0.5)
-        });
+      // Définir le volume initial à 0
+      video.volume = 0;
 
-        video.addEventListener("mouseleave", () => {
-          console.log("mouseleave");
-          // video.volume = 0; // Revenir à 0 quand la souris quitte
-        });
+      video.addEventListener("mouseenter", () => {
+        console.log("mouseenter");
+        // video.volume = 0.5; // Volume à 50% (0.5)
+      });
+
+      video.addEventListener("mouseleave", () => {
+        console.log("mouseleave");
+        // video.volume = 0; // Revenir à 0 quand la souris quitte
+      });
     },
   };
 })();
